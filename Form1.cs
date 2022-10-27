2e1219ecbc31f21505a82c41ef7109f6f6a2cc78
Ford25 <99024566+Ford25@users.noreply.github.com>
2022-10-27 15:29:58 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MoratallaLabMidterm
+{
+    public partial class FrmBasicThread : Form
+    {
+        
+
+
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+
+            //Thread basicThread1 = Thread.CurrentThread;
+
+            //basicThread1.Name = "Thread A";
+
+
+           
+           
+
+
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ThreadStart th = new ThreadStart(MyThreadClass.Thread1);
+            Thread threadA = new Thread(th);
+            threadA.Name = "Thread A";
+
+            Thread threadB = new Thread(th);
+            threadB.Name = "Thread B";
+
+
+            threadA.Start();
+            threadB.Start();
+            threadA.Join();
+            threadB.Join();
+
+           
+            label1.Text = "-End Of Thread-";
+
+            
+        }
+
+        private void FrmBasicThread_Load(object sender, EventArgs e)
+        {
+
+
+            
+
+
+
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MoratallaLabMidterm
+{
+    class MyThreadClass
+    {
+
+
+        public static void Thread1()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+
+                Thread.Sleep(1500);
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process" + " = " + i);
+
+                
+
+            }
+        }
+    }
+
+
+  
+
+
+
+}
